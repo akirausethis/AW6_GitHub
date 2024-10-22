0eed2490841336c59abf8be643426b952b09b5ab
Kelvin Marcello Piter | @ayyakira_ <133009613+akirausethis@users.noreply.github.com>
2024-10-22 10:56:39 +0700

    Add File

---
+++ app/src/main/java/com/example/aw6_github/AW6_Github.kt
@@ -0,0 +1,47 @@
+package com.example.aw6_github
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aw6_github.ui.theme.AW6_GitHubTheme
+
+class AW6_Github : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AW6_GitHubTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    AW6_GitHubTheme {
+        Greeting2("Android")
+    }
+}
